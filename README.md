802b20f3a583b891704eee9043e78f8933c83502
Luca <example@gmail.com>
2025-02-05 20:28:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# `CommonLibF4 NG - Template`
+
+## About
+
+This is a template for my CommonLibF4 NG project, designed to be a simple and straightforward way to start developing script extender mods for Fallout 4.
+
+## Build Dependencies
+
+- [Visual Studio Community 2022 Build Tools 17.12](https://learn.microsoft.com/visualstudio/releases/2022/release-history#evergreen-bootstrappers)
+- [mmio](https://github.com/Ryan-rsm-McKenzie/mmio)
+- [spdlog](https://github.com/gabime/spdlog)
+
+## Development
+
+- [CMake 3.30](https://cmake.org)
+- [vcpkg](https://github.com/microsoft/vcpkg)
+  - Create an environmental variable called `VCPKG_ROOT` where the value is the path to your vcpkg installation.
+
+## End User Dependencies
+
+- [Address Library for F4SE Plugins](https://www.nexusmods.com/fallout4/mods/47327)
+- [VR Address Library for F4SEVR Plugins](https://www.nexusmods.com/fallout4/mods/64879) (Optional)
+- [F4SE/VR](https://f4se.silverlock.org)
+
+## Development
+
+- [Address Library for F4SE Plugins](https://www.nexusmods.com/fallout4/mods/47327)
+- [VR Address Library for F4SEVR Plugins](https://www.nexusmods.com/fallout4/mods/64879) (Optional)
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+F4SE_EXPORT constinit auto F4SEPlugin_Version = []() noexcept {
+	auto data = F4SE::PluginVersionData();
+
+	data.AuthorName(Plugin::AUTHOR);
+	data.PluginName(Plugin::NAME);
+	data.PluginVersion(Plugin::VERSION);
+
+	data.UsesAddressLibrary(true);
+	data.IsLayoutDependent(true);
+	data.UsesSigScanning(false);
+	data.HasNoStructUse(false);
+
+	data.CompatibleVersions({ F4SE::RUNTIME_LATEST_NG });
+	return data;
+}();
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a_f4se, F4SE::PluginInfo* a_info)
+{
+	logger::info("{} v{}", Plugin::NAME, Plugin::VERSION);
+
+	a_info->infoVersion = F4SE::PluginInfo::kVersion;
+	a_info->name = Plugin::NAME.data();
+	a_info->version = Plugin::VERSION.major();
+
+	if (a_f4se->IsEditor()) {
+		logger::critical("Loading in editor is not supported");
+		return false;
+	}
+
+	const auto version = a_f4se->RuntimeVersion();
+	if (version < REL::Relocate(F4SE::RUNTIME_LATEST_OG, F4SE::RUNTIME_LATEST_NG)) {
+		logger::critical("Unsupported runtime v{}", version.string());
+		return false;
+	}
+
+	return true;
+}
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f4se)
+{
+	F4SE::Init(a_f4se);
+
+	logger::info("Loaded");
+
+	return true;
+}
